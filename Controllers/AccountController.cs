e1319d17d12621bbc2d4be171fd05093e8ff2dce
YOUR MOVIE PLUG <YOUR MOVIE PLUG@DESKTOP-MRDVUB1>
2025-01-04 23:48:21 +0300

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,131 @@
+﻿using HealthManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using HealthManagementSystem;
+
+
+namespace HealthManagementSystem.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        HealthcareDBEntities db = new HealthcareDBEntities();
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string Username, string Password)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Username == Username && u.Password == Password);
+            if (user != null)
+            {
+                Session["UserID"] = user.UserID;
+                Session["Username"] = user.Username;
+                return RedirectToAction("Index");
+            }
+
+
+            ViewBag.Error = "Invalid Username or Password";
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Index(string searchQuery)
+        {
+            var patients = db.Patients.AsQueryable();
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                patients = patients.Where
+                    (p => p.FullName.ToLower().Contains(searchQuery.ToLower()) || p.Address.ToLower().Contains(searchQuery.ToLower()));
+
+            }
+            return View(patients.ToList());
+
+
+        }
+        public ActionResult Details(int id)
+        {
+            var patient = db.Patients.Find(id);
+            if(patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        // GET: Account/Edit/5
+        [HttpGet]
+public ActionResult Edit(int id)
+{
+    var patient = db.Patients.Find(id);
+    if (patient == null)
+    {
+        return HttpNotFound();
+    }
+    return View(patient);
+}
+
+// POST: Account/Edit/5
+[HttpPost] [ValidateAntiForgeryToken]
+public ActionResult Edit(Patient patient)
+{
+    if (ModelState.IsValid)
+    {
+        db.Entry(patient).State = System.Data.Entity.EntityState.Modified;
+        db.SaveChanges();
+        TempData["Message"] = "Patient details updated successfully!";
+        return RedirectToAction("Index");
+    }
+    return View(patient);
+}
+       
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var patient = db.Patients.Find(id);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HealthManagementSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HealthManagementSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd7fb69a-3b98-4d73-9b20-0bfed9da079a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
